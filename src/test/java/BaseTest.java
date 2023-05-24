8aa11db80f5d0959917ca5309e1e30f21acb9aff
Svetlana <sveta_36@list.ru>
2023-05-24 23:03:35 +0400

    add solution

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,15 @@
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
 
+    static Faker faker;
+
     @BeforeAll
     public static void setBaseUrl () {
         RestAssured.baseURI = "https://api.openweathermap.org/data/2.5";
+        faker = new Faker();
+
     }
 }
--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -3,8 +3,10 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.Matchers.equalTo;
 
 public class WeatherTest extends BaseTest {
@@ -21,11 +23,98 @@ public class WeatherTest extends BaseTest {
     }
 
     @Test
-    public void shouldGetByCity() {
-        requestSpec
+    @DisplayName("Get forecast by City (correct name)")
+    public void shouldGetByCityPositive() {
+        step("Get forecast for London", () -> {
+              requestSpec
                 .get("/weather?q=London")
                 .then()
                 .assertThat()
                 .body("base", equalTo("stations"));
+        });
     }
+    @Test
+    @DisplayName("Get forecast by City (random name)")
+    public void shouldGetByRandomCity() {
+
+        String city = faker.address().city();
+                //"Abu Dhabi";
+        System.out.println(city);
+        step("Get forecast by City", () -> {
+            requestSpec
+                    .get("/weather?q={city}", city)
+                    .then()
+                    .assertThat()
+                    .log().body()
+                    .body("name", equalTo(city));
+        });
+    }
+
+    @Test
+    @DisplayName("Get forecast by correct ID")
+    public void shouldGetByIdPositive() {
+        requestSpec
+                .get("/weather?id=499068")
+                .then()
+                .assertThat()
+                .body("name", equalTo("Samara Oblast"));
+    }
+    @Test
+    @DisplayName("Get forecast by incorrect ID")
+    public void shouldGetByIdNegative() {
+        requestSpec
+                .get("/weather?id=499061")
+                .then()
+                .assertThat()
+                .statusCode(404)
+                .log().body()
+                .body("message", equalTo("city not found"));
+    }
+    @Test
+    @DisplayName("Get forecast by longitude and latitude")
+    //longitude and latitude
+    public void shouldGetByCoordinatePositive() {
+        requestSpec
+                .get("/weather?lat=48.8534&lon=2.3488")
+                .then()
+                .assertThat()
+                .body("name", equalTo("Paris"));
+    }
+    @Test
+    @DisplayName("Get forecast only by latitude")
+    public void shouldGetByCoordinateNegative1() {
+        requestSpec
+                .get("/weather?lat=43")
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .log().body()
+                .body("message", equalTo("Nothing to geocode"));
+    }
+
+    @Test
+    @DisplayName("Get forecast by incorrect coordinate")
+    public void shouldGetByCoordinateNegative2() {
+        requestSpec
+                .get("/weather?lat=111111&lon=2222222")
+                .then()
+                .assertThat()
+                .statusCode(400)
+                .log().body()
+                .body("message", equalTo("wrong latitude"));
+    }
+    @Test
+    @DisplayName("Get humidity")
+    public void shouldGetHumidityByCity() {
+
+        int airHumidity = requestSpec
+                .get("/weather?q=Rome")
+                .then()
+                .log().body()
+                .spec(responseWeather)
+                .body("name", equalTo("Rome"))
+                .extract()
+                .path("main.humidity");
+        }
+
 }
